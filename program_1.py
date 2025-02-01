6b7c218c5413dbc0a060359ed684c6c7589f264b
RayMcMillin <ramcmillin@students.unwsp.edu>
2025-01-31 21:55:01 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,5 +1,8 @@
 def personal_information():
-    print('...')
+    print("Name: Ray McMillin")
+    print("Address: 123 Birch Lane, St. Paul, MN 55101")
+    print("Phone Number: (612)-555-1234")
+    print("College Major: Business")
 
 
 # Line which calls the above function.
